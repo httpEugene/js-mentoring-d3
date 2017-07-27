f80193cc2baba5fb16945754ef17634b29a5100f
httpEugene <eugen.kiselov@gmail.com>
2017-07-27 22:44:13 +0300

    Update app.js

--- practice/basics/app.js
+++ practice/basics/app.js
@@ -1,17 +1,1 @@
-d3.select('svg')
-    	.select('circle')
-    	.attr('fill', 'green')
-    	.update()
-    	.append('rect')
-		.attr("x", 100)
-    	.attr("y", 100)
-    	.attr("width", 50)
-    	.attr("height", 100);;
-    
 
-// d3.select('svg')
-// 	.append('rect')
-// 		.attr("x", 100)
-//     	.attr("y", 100)
-//     	.attr("width", 50)
-//     	.attr("height", 100);
